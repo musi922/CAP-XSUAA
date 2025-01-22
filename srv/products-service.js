7b8d986c3e67927adf61fc6be147d7d454fbd481
Richard Musime <richardmusime6@gmail.com>
2025-01-22 17:02:04 +0200

    added changes

--- srv/products-service.js
+++ srv/products-service.js
@@ -1,7 +1,19 @@
 module.exports = srv => {
+    //srv means service
     console.log(`Service name: ${srv.name} is served at ${srv.path}` );
-    //xs means give me the data you got 
+    //xs means give me the data you got if you go on this path give me data you got
     srv.after('READ', 'Products', xs=>{
         console.log(xs)
+        const newProducts = [];
+        xs.forEach(x=>{
+            if (x.stock > 3){ 
+                x.name = `(10% off!) ${x.name}`;
+            }
+            newProducts.push(x);
+
+        })
+        console.log(newProducts);
+        
+        return newProducts
     })
 }
